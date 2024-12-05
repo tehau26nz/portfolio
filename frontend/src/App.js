04ef454227517d53ae2aca809b1a67cfd0090cc3
Tehaunui Shing Soi <tehaunuiss26@gmail.com>
2024-12-06 11:34:56 +1300

    Added react-router and experiences

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route >
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
