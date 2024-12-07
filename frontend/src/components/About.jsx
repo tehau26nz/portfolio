bac504530cab66e25e734db08fca1766951b8dff
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2024-12-07 18:42:58 +1300

    Added profile styling and logos

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import styles from "../components/about.module.css";
 
 const About = () => {
@@ -17,23 +18,74 @@ const About = () => {
       <h1 className={`${styles.title} ${styles.fade}`}>
         {translations[currentIndex]}
       </h1>
-      <div className={styles.intro}>
-        <h2>I'm Tehaunui</h2>
-        <div>
-          <img
-            className={styles.avatar}
-            src="https://avatars.githubusercontent.com/u/92765882?v=4"
-            alt="profile"
-          />
+      <Container className={styles.introCard}>
+        <Row className="justify-content-md-center" gap={3}>
+          <Col md="auto" className="mb-3">
+            <img
+              src="https://media.licdn.com/dms/image/v2/D4D03AQFpZjvOtH4YGw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1677642634851?e=1738800000&v=beta&t=Wl_fkuTowIJT2WBWihRPmInKneoYKB1UGrNkaSXDVx4"
+              alt="profile"
+              className={styles.avatar}
+            ></img>
+          </Col>
+          <Col lg="auto">
+            <h2 className="text-center">I'm Tehaunui</h2>
+            <Card className={styles.introDescription}>
+              <Card body className="justify-content p-3 m-1">
+                Currently a web developer at the Ministry of Education. I enjoy
+                being involved in front-end, back-end development, and build
+                intuitive web apps, websites and dev related projects.
+              </Card>
+            </Card>
+          </Col>
+        </Row>
+        <div className="text-center mt-5">
+          <h3>Skills</h3>
         </div>
-        <p className={styles.intro}>
-          Currently a web developer at the Ministry of Education
-          <br />
-          I enjoy being involved in front-end, back-end development, <br />
-          and build intuitive web apps, websites and dev related projects.{" "}
-          <br />
-        </p>
-      </div>
+        <Row
+          className="justify-content-center"
+          xs={true}
+          sm={true}
+          md={true}
+          lg={12}
+        >
+          <Col xs className="m-1">
+            <img src="/assets/logos/html-5.png" alt="HTML-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/css-3.png" alt="CSS-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/js.png" alt="JavaScript-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/typescript.png" alt="Typescript-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/react.png" alt="React-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/node-js.png" alt="NodeJS-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/php.png" alt="PHP-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/terminal.png" alt="Terminal-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/docker.png" alt="Docker-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img src="/assets/logos/github.png" alt="GitHub-logo"></img>
+          </Col>
+          <Col xs className="m-1">
+            <img
+              src="/assets/logos/vscode.png"
+              alt="Visual-Studio-Code-logo"
+            ></img>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
